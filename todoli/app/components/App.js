a299ad05df6d805610ac9b540aaf2a6a18c5324d
cglotr <amir.ariffin.920404@gmail.com>
2019-01-01 19:07:04 +0800

    extends component

--- todoli/app/components/App.js
+++ todoli/app/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { ScrollView, View } from 'react-native'
 import Footer from './Footer'
 import Title from './Title'
@@ -6,48 +6,50 @@ import Todo from './Todo'
 import { PADDING } from '../constants/dimensions'
 import Input from '../containers/Input'
 
-const App = ({ deleteCheckedTodos, deleteTodo, todos, toggleTodo }) => {
-  return (
-    <View style={{
-      alignItems: 'stretch',
-      flex: 1
-    }}>
-      <View style={{
-        alignItems: 'center'
-      }}>
-        <Title />
-      </View>
-      <View style={{
-        alignItems: 'center'
-      }}>
-        <Input />
-      </View>
-      <View style={{
-        flex: 1,
-        paddingBottom: PADDING,
-        paddingTop: PADDING
-      }}>
-        <ScrollView>
-          {todos.map((todo) => {
-            return (
-              <Todo
-                deleteTodo={deleteTodo}
-                key={todo.id}
-                todo={todo}
-                toggleTodo={toggleTodo}
-              />
-            )
-          })}
-        </ScrollView>
-      </View>
-      <View style={{
-        alignItems: 'center',
-        paddingBottom: PADDING * 2
-      }}>
-        <Footer deleteCheckedTodos={deleteCheckedTodos} />
-      </View>
-    </View>
-  )
-}
+export default class extends Component {
+  render () {
+    const { deleteCheckedTodos, deleteTodo, todos, toggleTodo } = this.props
 
-export default App
+    return (
+      <View style={{
+        alignItems: 'stretch',
+        flex: 1
+      }}>
+        <View style={{
+          alignItems: 'center'
+        }}>
+          <Title />
+        </View>
+        <View style={{
+          alignItems: 'center'
+        }}>
+          <Input />
+        </View>
+        <View style={{
+          flex: 1,
+          paddingBottom: PADDING,
+          paddingTop: PADDING
+        }}>
+          <ScrollView>
+            {todos.map((todo) => {
+              return (
+                <Todo
+                  deleteTodo={deleteTodo}
+                  key={todo.id}
+                  todo={todo}
+                  toggleTodo={toggleTodo}
+                />
+              )
+            })}
+          </ScrollView>
+        </View>
+        <View style={{
+          alignItems: 'center',
+          paddingBottom: PADDING * 2
+        }}>
+          <Footer deleteCheckedTodos={deleteCheckedTodos} />
+        </View>
+      </View>
+    )
+  }
+}
