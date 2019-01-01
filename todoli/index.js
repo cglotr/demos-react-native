75d4f0edef6eaac39b5b464a0a3b33c5620b9575
cglotr <amir.ariffin.920404@gmail.com>
2019-01-01 19:03:39 +0800

    move app to index

--- todoli/index.js
+++ todoli/index.js
@@ -1,7 +1,24 @@
 /** @format */
 
+import React from 'react'
 import { AppRegistry } from 'react-native'
-import App from './App'
+import { ThemeProvider } from 'react-native-elements'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import App from './app/containers/App'
+import rootReducer from './app/reducers'
 import { name as appName } from './app.json'
 
-AppRegistry.registerComponent(appName, () => App)
+const store = createStore(rootReducer)
+
+AppRegistry.registerComponent(appName, () => {
+  return () => {
+    return (
+      <Provider store={store}>
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
+      </Provider>
+    )
+  }
+})
