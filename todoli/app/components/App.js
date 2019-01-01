c40602f82e0a0b5a681a8e2348b78bb433dc063f
cglotr <amir.ariffin.920404@gmail.com>
2019-01-01 22:43:23 +0800

    sqlite db

--- todoli/app/components/App.js
+++ todoli/app/components/App.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react'
 import { ScrollView, View } from 'react-native'
+import SQLite from 'react-native-sqlite-storage'
 import Footer from './Footer'
 import Title from './Title'
 import Todo from './Todo'
+import { DATABASE } from '../../config'
 import { PADDING } from '../constants/dimensions'
 import Input from '../containers/Input'
 
+let database
+
 export default class extends Component {
+  componentDidMount () {
+    SQLite.DEBUG(true)
+    SQLite.enablePromise(true)
+
+    SQLite.openDatabase({ name: DATABASE.FILE_NAME, location: 'default' })
+      .then((db) => {
+        database = db
+      })
+  }
+
   render () {
     const { deleteCheckedTodos, deleteTodo, todos, toggleTodo } = this.props
 
